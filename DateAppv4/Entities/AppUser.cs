8fa0f1950fa3e82c128b00a3ffbf78ca33bdd205
Lefa Nyathi <Sikamarazor>
2021-11-29 14:34:51 +0200

    Ready to commit

---
+++ DateAppv4/Entities/AppUser.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DateAppv4.Entities
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+        
+    }
+}
---
+++ DateAppv4/Entities/People.cs
@@ -0,0 +1,12 @@
+﻿namespace DateAppv4.Entities
+{
+    public class People
+    {
+
+        public int myId { get; set; }
+
+        public string name { get; set; }
+
+        public string surname { get; set; }
+    }
+}
